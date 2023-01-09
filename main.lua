5ce463e1b5150f1ecf9312dec496c112087be62e
maririn312 <khatanzorigb@gmail.com>
2023-01-10 01:36:34 +0800

    lua game pacman mode changes

--- main.lua
+++ main.lua
@@ -1,15 +1,46 @@
-
-
 _G.love = require("love")
 
 function love.load()
-  _G.number = 0
-end 
+  love.graphics.setBackgroundColor(0.5, 0.5, 1)
+
+  _G.pacman = {}
+  pacman.x = 200
+  pacman.y = 250
+  pacman.eat = false
+
+  _G.food = {
+    x = 600,
+    eaten = false
+  }
+end
 
 function love.update(dt)
-  number = number + 2
-end 
+  pacman.x = pacman.x + 1
+
+  if love.keyboard.isDown("a") then
+    pacman.x = pacman.x - 1
+  elseif love.keyboard.isDown("b") then
+    pacman.x = pacman.x + 1
+  elseif love.keyboard.isDown("y") then
+    pacman.y = pacman.y + 1
+  elseif love.keyboard.isDown("d") then
+    pacman.y = pacman.y - 1
+  end
+
+  if pacman.x >= food.x + 20 then
+    _G.food_eaten = true
+  end
+
+end
 
 function love.draw()
-  love.graphics.rectangle("line", 10, 200, 100, 50)
+
+  if not food.eaten then
+    love.graphics.setColor(0, 0, 0)
+    love.graphics.rectangle("fill", food.x, 200, 70, 70)
+  end
+
+  love.graphics.setColor(1, 0.7, 0.1)
+  love.graphics.arc("fill", pacman.x, pacman.y, 60, 1, 5)
 end
+
